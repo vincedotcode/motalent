b0cce46c389ac5dbce46ff00ce50ec6553228be3
Vince Erkadoo <vince.erkadoo@sdworx.com>
2024-08-03 15:44:22 +0400

    service worker fixes

--- client/src/components/FirebaseForeground.tsx
+++ client/src/components/FirebaseForeground.tsx
@@ -6,7 +6,18 @@ import useFCMToken from '@/hooks/useFCMToken';
 
 const FirebaseForeground = () => {
   const { token, notificationPermissionStatus } = useFCMToken();
+  useEffect(() => {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.register('/firebase-messaging-sw.js')
+        .then(registration => {
+          console.log('Service Worker registered with scope:', registration.scope);
+        }).catch(error => {
+          console.error('Service Worker registration failed:', error);
+        });
+    }
+  }, []);
 
+  
   useEffect(() => {
     if (typeof window !== 'undefined' && 'serviceWorker' in navigator) {
       if (notificationPermissionStatus === 'granted') {
