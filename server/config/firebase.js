b71fc133302b1369998278c975d05cd0b9b550a5
Vince Erkadoo <vince.erkadoo@sdworx.com>
2024-08-03 10:47:41 +0400

    changes in prod environment

--- server/config/firebase.js
+++ server/config/firebase.js
@@ -1,6 +1,14 @@
-// firebaseAdmin.js
 import admin from 'firebase-admin';
-import serviceAccount from './servicekey.json' assert { type: 'json' };
+import dotenv from 'dotenv';
+dotenv.config();
+
+let serviceAccount;
+
+if (process.env.NODE_ENV === 'production') {
+  serviceAccount = require('/etc/secrets/servicekey.json');
+} else {
+  serviceAccount = require(process.env.FIREBASE_SERVICE_KEY_PATH);
+}
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
